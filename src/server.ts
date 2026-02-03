7c1946e349f9eb0ec43e8e9b14feb08a19e52ea2
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-02-03 12:50:27 -0300

    feat: add error handling

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import { routes } from "./routes";
+import { errorHandling } from "./middlewares/errorHandling";
 
 const server = express();
 const PORT = process.env.PORT || 3000;
 
 server.use(express.json());
 server.use(routes);
+server.use(errorHandling);
 
 server.listen(PORT, () => {
   console.log(`Server running in the PORT:${PORT}`);
