6868777d0ef0a87ced0f52a611a62b1990dbc2f9
Cicero Eduardo <ciceroeduardobarbosa@hotmail.com>
2026-02-25 23:24:57 -0300

    feat: add task routes

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { TasksControllers } from "../controllers/tasksControllers";
 
 const tasksRoutes = Router();
+const tasksControllers = new TasksControllers();
 
-tasksRoutes.post("/tasks");
-tasksRoutes.get("/tasks");
-tasksRoutes.put("/tasks/:teamId");
-tasksRoutes.delete("/tasks/:teamId");
+tasksRoutes.post("/", tasksControllers.create);
+tasksRoutes.get("/",tasksControllers.read);
+tasksRoutes.put("/:teamId", tasksControllers.update);
+tasksRoutes.delete("/:teamId", tasksControllers.delete);
 
 export { tasksRoutes };
